af764815887afac49863ba08014861b6aada800e
neekodavinci <46725236+neekodavinci@users.noreply.github.com>
2019-01-15 14:20:26 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,25 @@
+/*******************************
+*Hello.java
+********************************
+*Lab 1
+*_______________________________
+*Dominic Mason
+*January 15, 2019
+*CMSC-255, Section 002
+*******************************/
+
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Dominic Mason");
+		System.out.println("Hello.java");
+		System.out.println("Lab 1");
+		System.out.println("January 15, 2019");
+		System.out.println("CMSC-255, Section 002");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
